5bde673fbb3ff79bfd84f2eda6c20e03a4b23750
Elizabeth Souza <110788212+SouzaBeth@users.noreply.github.com>
2022-11-02 22:26:41 -0300

    first commit

---
+++ PY AV2 Q1.py
@@ -0,0 +1,6 @@
+bmaior=float(input("Digite o valor da base menor: "))
+bmenor=float(input("Digite o valo da base menor: "))
+h = float(input("Digite o valor da altura: "))
+área=((bmaior+bmenor)*h)/2
+print(f'o valor da área desse trapezio {área:.2f} unidades de área')
+
